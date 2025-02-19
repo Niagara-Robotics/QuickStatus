403bf05d7aa6f12c182fd0a91f098cc51d8894f3
40F4 <ian.stewart@cogeco.ca>
2025-02-19 13:46:59 -0500

    Module installation file
    separated the stupid installation thing because its way easier lmao
    changed how window refreshing works a bit
    tab widgets look less odd now

--- QuickStatus.py
+++ QuickStatus.py
@@ -1,49 +1,12 @@
-# auto import stuff
-import sys, os, random, time
-from shutil import which
+# import stuff
+import sys, os, random, time, toml
 from math import *
-import PyQt6
-can_run = True
-pip = which('pip') is not None # do you have pip
-install_toml = ""
-install_pyqt6 = ""
-
-# toml
-try: import toml
-except:
-    if pip == False:
-        print('Missing toml module, can\'t install because you don\'t have pip\nhttps://pip.pypa.io/en/stable/installation/')
-        sys.exit()
-    while not (install_toml.capitalize() == "Y" or install_toml.capitalize() == "N"):
-        install_toml = input("You don't have the toml module installed! Install it now? (Y/n) ")
-    if install_toml.capitalize() == "Y":
-        os.system('pip install toml')
-        import toml
-    else:
-        can_run = False
 # pyqt6
-try:
-    from PyQt6.QtCore import *
-    from PyQt6.QtGui import *
-    from PyQt6.QtWidgets import *
-    from PyQt6.QtGui import QPainter, QColor, QPen, QFont, QFontMetrics
-    from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
-except:
-    if pip == False:
-        print('Missing PyQt6 module, can\'t install because you don\'t have pip\nhttps://pip.pypa.io/en/stable/installation/')
-        sys.exit()
-    while not (install_pyqt6.capitalize() == "Y" or install_pyqt6.capitalize() == "N"):
-        install_pyqt6 = input("You don't have the PyQt6 module installed! Install it now? (Y/n) ")
-    if install_pyqt6.capitalize() == "Y":
-        os.system('pip install pyqt6')
-        os.system('python3 pyqt.py') # restart program, pyqt6 breaks when trying to import after installation for some reason
-        sys.exit()
-    else:
-        can_run = False
-
-if can_run == False:
-    print("mf really neglected to install the modules i NEED to run. shutting the whole operation down.")
-    sys.exit()
+from PyQt6.QtCore import *
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QPainter, QColor, QPen, QFont, QFontMetrics
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
 
 def resource_path(relative_path):
     # absolute file paths
@@ -52,20 +15,11 @@ def resource_path(relative_path):
 
 # setup variables
 start_time = time.time()
-w = 640; h = 480
 title = 'QuickStatus'
+refresh = 10
 # statuses for testing only, recieve data later
 things = ["thingamajig", "this is the end for me", "i woke up fresh as hell on a monday", "whatchamacallit", "among Gus", "blah blah blah the mitochondria is the powerhouse of the cell", "what is good homies", "thingamajig 2", "whatchamacallit 2", "among Gus 2", "blah blah blah the mitochondria is the powerhouse of the cell 2", "what is good homies 2", "LOOK OUT FROM ABOVE!!!", "My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink. I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up. I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone."]
 values = [0, 4, 3, 3, 1, 1, 2, 2, 3, 3, 4, 4, 1, 0]
-# wheel rot
-base_rot = 45
-fart = 0
-wheel_rot = [15,-5,86,-123]
-velocities = [0.59,-0.4,1,-0.1]
-targ_velocities = [0.7,-0.74,1,-0.7]
-powers = [0.76,-0.5,0.96,-0.6]
-wheel_status = [0,1,2,4]
-base_status = 3
 # colour values
 accent_colour = QColor("#0779FF")
 caution_colour = QColor("#FFC600")
@@ -165,39 +119,31 @@ class PagesWindow(QWidget):
 
         restoreWindow(self)
 
+        # create tab widget
         widget = QWidget()
         self.layout = QGridLayout(widget)
-        self.layout.setContentsMargins(0,0,0,10)
-        page = 1
-        max_page = 1
-        text = QLabel("Page " + str(page) + "/" + str(max_page))
-        text.setAlignment(Qt.AlignmentFlag.AlignBottom)
-        '''layout.addWidget(text,0,0)
-        next_page = QPushButton("Next Page")
-        last_page = QPushButton("Last Page")
-        layout.addWidget(next_page,0,2, Qt.AlignmentFlag.AlignBottom)
-        layout.addWidget(last_page,0,1, Qt.AlignmentFlag.AlignBottom)'''
+        self.layout.setContentsMargins(0,0,0,5)
         self.tabs = QTabWidget()
         
+        # align config
         if config['tabs']['align'] == 'bottom': self.tabs.setTabPosition(self.tabs.TabPosition.South)
         if config['tabs']['align'] == 'top': self.tabs.setTabPosition(self.tabs.TabPosition.North)
         if config['tabs']['align'] == 'right': self.tabs.setTabPosition(self.tabs.TabPosition.East)
         if config['tabs']['align'] == 'left': self.tabs.setTabPosition(self.tabs.TabPosition.West)
         
+        # create tabs
         for i in self.tablist:
             if i == 'claw': self.ClawTab()
             if i == 'robot': self.RobotStateTab()
             if i == 'status': self.StatusTab()
 
         self.layout.addWidget(self.tabs)
-
         self.setLayout(self.layout)
 
         self.setWindowTitle(title + ' Tabs')
 
     def RobotStateTab(self):
         self.tabs.addTab(RobotStateWidget(), "Robot State")
-        #RobotStateWidget().setAutoFillBackground(True)
     def StatusTab(self):
         self.tabs.addTab(StatusWindow(), "Status Lights")
     def ClawTab(self):
@@ -207,6 +153,18 @@ class PagesWindow(QWidget):
         if config['windows']['save-window-positions']:
             self.settings.setValue( "windowScreenGeometry", self.saveGeometry() )
         e.accept()
+    def changeEvent(self, e):
+        hl = app.palette().color(QPalette.ColorRole.WindowText)
+        hl.setAlpha(25)
+        hl = hl.name().strip('#')
+        self.tabs.setStyleSheet(
+        f"""
+        QTabWidget::pane {{
+        border-bottom: 1px solid;
+        border-color: #33{hl};
+        margin-bottom: 5px;
+        }}
+        """)
 
 #create window with status lights
 class StatusIndicatorWidget(QWidget):
@@ -215,7 +173,7 @@ class StatusIndicatorWidget(QWidget):
         self.num_circles = len(things)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
-        self.timer.start(int(1000/config['status']['update-rate']))
+        self.timer.start(refresh)
 
     # scrolling setup
     def minimumSizeHint(self):
@@ -307,10 +265,18 @@ class RobotStateWidget(QWidget):
         self.settings = QSettings('QuickStatus', str(MainWindow.windows))
         self.setWindowTitle(title + ' Robot State')
         self.resize(500,500)
+
+        # Adjust the timer interval to match the monitor's refresh rate
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
-        self.timer.start(int(1000/config['robot']['update-rate']))
-        self.br = 0
+        self.timer.start(refresh)
+        self.base_rot = 45
+        self.wheel_rot = [15,-5,86,-123]
+        self.velocities = [0.59,-0.4,1,-0.1]
+        self.targ_velocities = [0.7,-0.74,1,-0.7]
+        self.powers = [0.76,-0.5,0.96,-0.6]
+        self.wheel_status = [0,1,2,4]
+        self.base_status = 3
     # draw status lights
     def paintEvent(self, event):
         #Setup
@@ -321,7 +287,6 @@ class RobotStateWidget(QWidget):
         foreground_colour = palette.color(QPalette.ColorRole.Text)
         foreground_colour.setAlpha(255)
         colour_chart = [foreground_colour, accent_colour, caution_colour, warning_colour, death_colour]
-
         size = self.size()
         w = size.width()
         h = size.height()
@@ -332,9 +297,7 @@ class RobotStateWidget(QWidget):
 
         # Rotate base
         qp.translate(cw,ch)
-        qp.rotate(self.br)
-        self.br += 0.2
-        if config['robot']['base-lock']: self.br = 0
+        qp.rotate(self.base_rot)
 
         #Base
         rx = rs*2
@@ -344,10 +307,10 @@ class RobotStateWidget(QWidget):
 
         pen = QPen()
         pen.setStyle(Qt.PenStyle.SolidLine)
-        pen.setColor(colour_chart[base_status])
+        pen.setColor(colour_chart[self.base_status])
         pen.setWidthF(cw/60)
         qp.setPen(pen)
-        qp.setBrush(colour_chart[base_status])
+        qp.setBrush(colour_chart[self.base_status])
         qp.drawRect(QRectF(irx1, iry1, rx, ry))
 
         qp.setBrush(Qt.BrushStyle.NoBrush)
@@ -355,13 +318,13 @@ class RobotStateWidget(QWidget):
 
         '''Draw Wheel'''
         for i in range(4):
-            wheel_rot[i] -= 1
+            self.wheel_rot[i] -= 1
             if i%2: irx = -irx1
             else: irx = irx1
             if i>1: iry = -iry1
             else: iry = iry1
             qp.translate(irx,iry)
-            if not config['robot']['wheel-lock']: qp.rotate(wheel_rot[i])
+            if not config['robot']['wheel-lock']: qp.rotate(self.wheel_rot[i])
             else: qp.rotate(-self.br)
             #BG Circle
             pen.setStyle(Qt.PenStyle.NoPen)
@@ -391,13 +354,13 @@ class RobotStateWidget(QWidget):
             qp.drawChord(QRectF(-cs,-cs,cs*2,cs*2),(-la*8)+(1440),(-(180-la)*16))
             
             qp.setPen(QPen(velocity_colour, bd, cap=Qt.PenCapStyle.FlatCap))
-            qp.drawLine(QLineF(-bd2, 0, -bd2, -cs*velocities[i]))
+            qp.drawLine(QLineF(-bd2, 0, -bd2, -cs*self.velocities[i]))
             
             qp.setPen(QPen(power_colour, bd, cap=Qt.PenCapStyle.FlatCap))
-            qp.drawLine(QLineF(bd2, 0, bd2, -cs*powers[i]))
+            qp.drawLine(QLineF(bd2, 0, bd2, -cs*self.powers[i]))
             
             #Circle
-            pen.setColor(colour_chart[wheel_status[i]])
+            pen.setColor(colour_chart[self.wheel_status[i]])
             pen.setWidthF(cw/80)
             qp.setPen(pen)
             qp.setBrush(Qt.BrushStyle.NoBrush)
@@ -405,15 +368,17 @@ class RobotStateWidget(QWidget):
 
             #Target velocity
             td = (cw/40)
-            qp.translate(-cs/2.4,-cs*targ_velocities[i])
+            qp.translate(-cs/2.4,-cs*self.targ_velocities[i])
             qp.setPen(Qt.PenStyle.NoPen)
             qp.setBrush(QBrush(foreground_colour))
             qp.drawPolygon([QPointF(0,td),QPointF(0,-td),QPointF(sqrt(((td*2)**2)-(td/2)**2),0)])
-            qp.translate(cs/2.4,cs*targ_velocities[i])
+            qp.translate(cs/2.4,cs*self.targ_velocities[i])
 
-            if not config['robot']['wheel-lock']: qp.rotate(-wheel_rot[i])
-            else: qp.rotate(self.br)
+            if not config['robot']['wheel-lock']: qp.rotate(-self.wheel_rot[i])
+            else: qp.rotate(self.base_rot)
             qp.translate(-irx,-iry)
+        self.base_rot += 0.2
+        if config['robot']['base-lock']: self.base_rot = 0
 
     def closeEvent(self, e):
         if config['windows']['save-window-positions']:
@@ -428,7 +393,7 @@ class ClawStateWidget(QWidget):
         self.resize(500,500)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update)
-        self.timer.start(int(1000/config['claw']['update-rate']))
+        self.timer.start(refresh)
         claw = open(resource_path('assets/claw/claw.coords'))
         self.clawv = []
         clawl = []
@@ -518,6 +483,7 @@ if __name__ == '__main__':
         config.write('''[windows]
     save-window-positions = true
     widgets = [ ['status'], ['robot', 'claw'] ]
+    data-rate = 5
     
 [tabs]
     align = 'bottom' # Align tab bar to either 'bottom', 'top', 'left', or 'right'
@@ -526,12 +492,10 @@ if __name__ == '__main__':
     scroll-horizontal = false
     scroll-vertical = true
     blink-speed = 0.75 # Blink speed in seconds
-    update-rate = 100 # Widget refresh rate in Hz
 
 [robot]
     base-lock = false
     wheel-lock = false
-    update-rate = 100 # Widget refresh rate in Hz
 
 [claw]
     update-rate = 100 # Widget refresh rate in Hz''')
