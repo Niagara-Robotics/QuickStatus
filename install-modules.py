8f9f5d642ad8049c6f07b3ac3ca7abc07d1c4443
40F4 <ian.stewart@cogeco.ca>
2025-02-22 11:20:31 -0500

    Replaced module install with requirements
    just use pip yourself lmao

--- install-modules.py
+++
@@ -1,8 +0,0 @@
-import os
-from shutil import which
-if which('pip') is not None:
-    os.system("pip install toml")
-    os.system("pip install pyqt6")
-    os.system("pip install robotpy")
-else:
-    print("bro does NOT have pip")
